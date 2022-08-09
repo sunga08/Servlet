ad795aceeec0ae7d039f464f211a94ae18c94c86
sunga <eert2260@naver.com>
2022-08-10 00:38:31 +0900

    Servlet - http request data

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,30 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username"); //쿼리파라미터 값 가져옴
+        System.out.println("username = " + username);
+
+        //헤더 정보
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        //http body
+        response.getWriter().write("hello "+username);
+    }
+}
